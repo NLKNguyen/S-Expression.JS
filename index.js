3274d7adce6c3f8870b045edaf6050f7b904d073
Nikyle Nguyen <NLKNguyen@MSN.com>
2020-11-26 20:02:09 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,135 @@
+'use strict'
+
+class SExpression {
+  truthy = ['true', '#t']
+  falsy = ['false', '#f']
+  nully = ['null', '#nil']
+
+  constructor (options = {}) {
+    this.truthy = options.truthy || this.truthy
+    this.falsy = options.falsy || this.falsy
+    this.nully = options.nully || this.nully
+  }
+
+  parse (str) {
+    const t = str.match(/\s*("[^"]*"|\(|\)|"|[^\s()"]+)/g)
+    let o, c, i
+    for (o, c = 0, i = t.length - 1; i >= 0; i--) {
+      let n,
+        ti = t[i].trim()
+      if (ti == '"') return
+      else if (ti == '(') (t[i] = '['), (c += 1)
+      else if (ti == ')') (t[i] = ']'), (c -= 1)
+      else if ((n = +ti) == ti) t[i] = n
+      else t[i] = "'" + ti.replace("'", "\\'") + "'"
+      if (i > 0 && ti != ']' && t[i - 1].trim() != '(') t.splice(i, 0, ',')
+      if (!c)
+        if (!o) o = true
+        else return
+    }
+    return c ? undefined : eval(t.join(''))
+  }
+
+  serialize (L) {
+    let s = ''
+    for (let i = 0, e = L.length; i < e; i++) s += (s ? ' ' : '') + L[i]
+    return '(' + s + ')'
+  }
+
+  isIdentifier (e) {
+    return (
+      e &&
+      !this.isExpression(e) &&
+      !this.isNumber(e) &&
+      !this.isString(e) &&
+      !this.isBoolean(e)
+    )
+  }
+
+  isExpression (e) {
+    return Array.isArray(e)
+  }
+
+  isBoolean (e) {
+    if (typeof e == 'string') {
+      for (let t of this.truthy) {
+        if (e === t) {
+          return true
+        }
+      }
+      for (let f of this.falsy) {
+        if (e === f) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  isTruthy (e) {
+    if (typeof e == 'string') {
+      for (let f of this.falsy) {
+        if (e === f) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  isNull (e) {
+    if (typeof e == 'string') {
+      for (let n of this.nully) {
+        if (e === n) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  isNumber (e) {
+    return typeof e === 'number'
+  }
+
+  isString (e) {
+    return typeof e === 'string' && e[0] === '"'
+  }
+
+  stringContent (e) {
+    return e.slice(1, a.length - 1)
+  }
+
+  first (e) {
+    if (Array.isArray(e)) {
+      return e[0]
+    }
+    return undefined
+  }
+
+  second (e) {
+    if (Array.isArray(e)) {
+      return e[1]
+    }
+    return undefined
+  }
+
+  third (e) {
+    if (Array.isArray(e)) {
+      return e[2]
+    }
+    return undefined
+  }
+
+  nth (e, n) {
+    if (Array.isArray(e)) {
+      return e[n - 1]
+    }
+    return undefined
+  }
+}
+
+// const S = new SExpression()
+
+// console.log(S.parse("(a-b true () '('1 2' . 3))"))
+module.exports = SExpression
---
+++ tests/all.js
@@ -0,0 +1,23 @@
+const test = require('tape')
+const SExpression = require('../index')
+
+test('flat list', function (t) {
+  const S = new SExpression()
+  t.plan(4)
+  t.deepEqual(S.parse('(1 2 3)'), [1, 2, 3])
+  t.deepEqual(S.serialize([1, 2, 3]), '(1 2 3)')
+
+  t.deepEqual(S.parse('(a 1 "cd ef ( )")'), ['a', 1, '"cd ef ( )"'])
+  t.deepEqual(S.serialize(['a', 1, '"cd ef ( )"']), '(a 1 "cd ef ( )")')
+})
+
+
+test('type checks', function (t) {
+  const S = new SExpression()
+  const node = S.parse('(a 1 "c d e" true #t false #f null #nil ())')
+  t.plan(5)
+  t.true(S.isIdentifier(node[0]))
+  t.true(S.isNumber(node[1])); t.false(S.isIdentifier(node[1]))
+  t.true(S.isString(node[2])); t.false(S.isIdentifier(node[2]))
+  // TODO: more tests
+})
