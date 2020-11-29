7d8148df45d52bcc09c4b1a4fecd0dd35442035b
Nikyle Nguyen <NLKNguyen@MSN.com>
2020-11-28 18:30:32 -0800

    feat: node checkers for tree walking

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
-
 class SExpr {
   truthy = ['true', '#t']
   falsy = ['false', '#f']
@@ -11,6 +10,7 @@ class SExpr {
     this.nully = options.nully || this.nully
   }
 
+  // @ref credit: https://rosettacode.org/wiki/S-expressions#JavaScript
   parse (str) {
     const t = str.match(/\s*("[^"]*"|\(|\)|"|[^\s()"]+)/g)
     let o, c, i
@@ -60,33 +60,78 @@ class SExpr {
     }
   }
 
-  expression () { // TODO: take vargs
+  expression (...exps) {
+    if (exps) {
+      return [...exps]
+    }
     return []
   }
 
-  isIdentifier (e) {
-    return (
+  isIdentifier (e, id = undefined) {
+    const isId =
       e &&
       !this.isExpression(e) &&
       !this.isNumber(e) &&
       !this.isString(e) &&
       !this.isBoolean(e)
-    )
+
+    if (id) {
+      return isId && e === id
+    }
+    return isId
   }
 
-  isExpression (e) {
-    return Array.isArray(e)
+  // isEqual (a, b) {
+  //   return deepEqual(a, b, { strict: true })
+  // }
+
+  isEqual (a, b) {
+    const aArray = Array.isArray(a)
+    const bArray = Array.isArray(b)
+    if (aArray != bArray) {
+      return false
+    }
+
+    if (!aArray) {
+      return a === b
+    }
+
+    if (a.length != b.length) {
+      return false
+    }
+
+    for (let i in a) {
+      if (!this.isEqual(a[i], b[i])) {
+        return false
+      }
+    }
+    return true
   }
 
-  isBoolean (e) {
-    if (typeof e == 'string') {
+  isExpression (e, s = undefined) {
+    const isExpr = Array.isArray(e)
+
+    if (s) {
+      return isExpr && isEqual(e, s)
+    }
+    return isExpr
+  }
+
+  isBoolean (e, b = undefined) {
+    if (typeof e === 'string' || e instanceof String) {
       for (let t of this.truthy) {
         if (e === t) {
+          if (b != undefined) {
+            return b == true
+          }
           return true
         }
       }
       for (let f of this.falsy) {
         if (e === f) {
+          if (b != undefined) {
+            return b == false
+          }
           return true
         }
       }
@@ -95,7 +140,7 @@ class SExpr {
   }
 
   isTruthy (e) {
-    if (typeof e == 'string') {
+    if (typeof e == 'string' || e instanceof String) {
       for (let f of this.falsy) {
         if (e === f) {
           return false
@@ -106,7 +151,7 @@ class SExpr {
   }
 
   isNull (e) {
-    if (typeof e == 'string') {
+    if (typeof e == 'string' || e instanceof String) {
       for (let n of this.nully) {
         if (e === n) {
           return true
@@ -117,15 +162,42 @@ class SExpr {
   }
 
   isNumber (e) {
-    return typeof e === 'number'
+    return typeof e === 'number' || e instanceof Number
   }
 
   isString (e) {
-    return typeof e === 'string' && e[0] === '"'
+    return (typeof e === 'string' || e instanceof String) && e[0] === '"'
   }
 
-  content (e) {
-    return e.slice(1, a.length - 1)
+  valueOf (e) {
+    if (typeof e === 'string' || e instanceof String) {
+      // maybe quoted string
+      if (e[0] === '"') {
+        return e.slice(1, e.length - 1)
+      }
+
+      // maybe null
+      for (let n of this.nully) {
+        if (e === n) {
+          return null
+        }
+      }
+
+      // maybe boolean
+      for (let t of this.truthy) {
+        if (e === t) {
+          return true
+        }
+      }
+      for (let f of this.falsy) {
+        if (e === f) {
+          return false
+        }
+      }
+    }
+
+    // maybe number or identifier
+    return e
   }
 
   first (e) {
@@ -149,6 +221,8 @@ class SExpr {
     return undefined
   }
 
+  // TODO: 4th -> 10th
+
   nth (e, n) {
     if (Array.isArray(e)) {
       return e[n - 1]
@@ -157,11 +231,40 @@ class SExpr {
   }
 }
 
-// const S = new SExpr()
+const S = new SExpr()
 
-// const node = S.expression()
-// node.push(S.identifier('add'), S.number(5), S.expression( S.identifier('event'), S.string('CASE')))) )
-// console.log(S.null())
-// console.log(S.boolean(true))
-// console.log(S.parse("(a-b true () '('1 2' . 3))"))
+const node = S.expression()
+node.push(
+  S.identifier('repeat'),
+  S.number(3),
+  S.expression(S.identifier('print'), S.string('Knock'))
+)
+
+const parsed = S.parse(`(repeat 3 (print "Knock"))`)
+
+console.assert(S.isEqual(node, parsed))
+
+console.log(node)
+
+// same as S.isIdentifier(node[0]) && node[0] === 'repeat'
+if (S.isIdentifier(S.first(node), 'repeat')) {  
+  if (S.isNumber(S.second(node))) {
+    const n = S.second(node) // same as node[1] or S.nth(node, 2)
+    const e = S.third(node) // same as node[2] or S.nth(node, 3)
+    if (S.isExpression(e)) {
+      if (S.isIdentifier(S.first(e), 'print') && S.isString(S.second(e))) {
+        for (let i = 0; i < n; ++i) {
+          console.log(S.valueOf(S.second(e))) // S.valueOf is necessary for obtaining String value
+        }
+      } else {
+        console.error(`'repeat' doesn't handle ${S.serialize(e)} expression`)
+      }
+    } else {
+      console.error(`'repeat' expects an expression to be repeated`)
+    }
+  } else {
+    console.error(`'repeat' expects a number of times to repeat an expression`)
+  }
+}
+
 module.exports = SExpr
--- tests/all.js
+++ tests/all.js
@@ -11,13 +11,74 @@ test('flat list', function (t) {
   t.deepEqual(S.serialize(['a', 1, '"cd ef ( )"']), '(a 1 "cd ef ( )")')
 })
 
+test('isEqual', function (t) {
+  const S = new SExpr()
+
+  t.plan(9)
+  t.true(S.isEqual(1, 1))
+  t.true(S.isEqual('a', 'a'))
+  t.true(S.isEqual([], []))
+  t.true(S.isEqual([1, [2, [3, ['a b c d']]]], [1, [2, [3, ['a b c d']]]]))
+
+  t.false(S.isEqual(1, 2))
+  t.false(S.isEqual('a', ''))
+  t.false(S.isEqual(['a'], []))
+  t.false(S.isEqual([1, [2, [3, ['a b c d']]]], [1, [2, [3, [4]]]]))
+
+  const node = S.expression()
+  node.push(
+    S.identifier('repeat'),
+    S.number(3),
+    S.expression(S.identifier('print'), S.string('Knock'))
+  )
+
+  const parsed = S.parse(`(repeat 3 (print "Knock"))`)
+
+  t.true(S.isEqual(node, parsed))
+})
 
 test('type checks', function (t) {
   const S = new SExpr()
   const node = S.parse('(a 1 "c d e" true #t false #f null #nil ())')
-  t.plan(5)
+  // const isExpectedType = (e, expectedPred) => {
+  //   for(let pred of [S.isIdentifier, S.isString, S.isNumber, S.isBoolean]) {
+  //     if (pred == expectedPred) {
+  //       t.true(pred(e))
+  //     } else {
+  //       t.false(pred(e))
+  //     }
+  //   }
+  // }
+  // t.plan(4)
+  // isExpectedType(node[0], S.isIdentifier)
+
+  t.plan(21)
+
   t.true(S.isIdentifier(node[0]))
-  t.true(S.isNumber(node[1])); t.false(S.isIdentifier(node[1]))
-  t.true(S.isString(node[2])); t.false(S.isIdentifier(node[2]))
-  // TODO: more tests
+  t.equal(S.valueOf(node[0]), 'a')
+  t.false(S.isString(node[0]))
+
+  t.true(S.isNumber(node[1]))
+  t.equal(S.valueOf(node[1]), 1)
+  t.false(S.isIdentifier(node[1]))
+
+  t.true(S.isString(node[2]))
+  t.equal(S.valueOf(node[2]), 'c d e')
+  t.false(S.isIdentifier(node[2]))
+  
+  t.true(S.isBoolean(node[3]))
+  t.true(S.valueOf(node[3]))
+  t.false(S.isIdentifier(node[3]))
+
+  t.true(S.isBoolean(node[4]))
+  t.true(S.valueOf(node[4]))
+  t.false(S.isString(node[4]))
+
+  t.true(S.isBoolean(node[5]))
+  t.false(S.valueOf(node[5]))
+  t.false(S.isString(node[5]))
+
+  t.true(S.isBoolean(node[6]))
+  t.false(S.valueOf(node[6]))
+  t.false(S.isString(node[6]))
 })
