d60a677794da7cc7f360f6fbb6084ae27520eb90
Nikyle Nguyen <NLKNguyen@MSN.com>
2022-10-06 21:42:23 -0700

    test: more cases

--- tests/all.js
+++ tests/all.js
@@ -1,47 +1,48 @@
-const test = require('tape')
-const SExpr = require('../index')
+const test = require("tape")
+const colorize = require("json-colorizer")
+const SExpr = require("../index")
 
-test('flat list', function (t) {
+test("flat list", function (t) {
   const S = new SExpr()
   t.plan(4)
-  t.deepEqual(S.parse('(1 2 3)'), [1, 2, 3])
-  t.deepEqual(S.serialize([1, 2, 3]), '(1 2 3)')
+  t.deepEqual(S.parse("(1 2 3)"), [1, 2, 3])
+  t.deepEqual(S.serialize([1, 2, 3]), "(1 2 3)")
 
-  t.deepEqual(S.parse('(a 1 "cd ef ( )")'), ['a', 1, '"cd ef ( )"'])
-  t.deepEqual(S.serialize(['a', 1, '"cd ef ( )"']), '(a 1 "cd ef ( )")')
+  t.deepEqual(S.parse('(a 1 "cd ef ( )")'), ["a", 1, '"cd ef ( )"'])
+  t.deepEqual(S.serialize(["a", 1, '"cd ef ( )"']), '(a 1 "cd ef ( )")')
 })
 
-test('isEqual', function (t) {
+test("isEqual", function (t) {
   const S = new SExpr()
 
   t.plan(9)
   t.true(S.isEqual(1, 1))
-  t.true(S.isEqual('a', 'a'))
+  t.true(S.isEqual("a", "a"))
   t.true(S.isEqual([], []))
-  t.true(S.isEqual([1, [2, [3, ['a b c d']]]], [1, [2, [3, ['a b c d']]]]))
+  t.true(S.isEqual([1, [2, [3, ["a b c d"]]]], [1, [2, [3, ["a b c d"]]]]))
 
   t.false(S.isEqual(1, 2))
-  t.false(S.isEqual('a', ''))
-  t.false(S.isEqual(['a'], []))
-  t.false(S.isEqual([1, [2, [3, ['a b c d']]]], [1, [2, [3, [4]]]]))
+  t.false(S.isEqual("a", ""))
+  t.false(S.isEqual(["a"], []))
+  t.false(S.isEqual([1, [2, [3, ["a b c d"]]]], [1, [2, [3, [4]]]]))
 
   const node = S.expression()
   node.push(
-    S.identifier('repeat'),
+    S.identifier("repeat"),
     S.number(3),
-    S.expression(S.identifier('print'), S.string('Knock'))
+    S.expression(S.identifier("print"), S.string("Knock"))
   )
 
-  const parsed = S.parse(`(repeat 3 (print "Knock"))`)
+  const ast = S.parse(`(repeat 3 (print "Knock"))`)
 
-  t.true(S.isEqual(node, parsed))
+  t.true(S.isEqual(node, ast))
 })
 
-test('type checks', function (t) {
+test("type checks", function (t) {
   const S = new SExpr()
   const node = S.parse('(a 1 "c d e" true #t false #f null #nil ())')
   // const isExpectedType = (e, expectedPred) => {
-  //   for(let pred of [S.isIdentifier, S.isString, S.isNumber, S.isBoolean]) {
+  //   for(let pred of [S.isAtom, S.isString, S.isNumber, S.isBoolean]) {
   //     if (pred == expectedPred) {
   //       t.true(pred(e))
   //     } else {
@@ -50,25 +51,25 @@ test('type checks', function (t) {
   //   }
   // }
   // t.plan(4)
-  // isExpectedType(node[0], S.isIdentifier)
+  // isExpectedType(node[0], S.isAtom)
 
   t.plan(21)
 
-  t.true(S.isIdentifier(node[0]))
-  t.equal(S.valueOf(node[0]), 'a')
+  t.true(S.isAtom(node[0]))
+  t.equal(S.valueOf(node[0]), "a")
   t.false(S.isString(node[0]))
 
   t.true(S.isNumber(node[1]))
   t.equal(S.valueOf(node[1]), 1)
-  t.false(S.isIdentifier(node[1]))
+  t.false(S.isAtom(node[1]))
 
   t.true(S.isString(node[2]))
-  t.equal(S.valueOf(node[2]), 'c d e')
-  t.false(S.isIdentifier(node[2]))
-  
+  t.equal(S.valueOf(node[2]), "c d e")
+  t.false(S.isAtom(node[2]))
+
   t.true(S.isBoolean(node[3]))
   t.true(S.valueOf(node[3]))
-  t.false(S.isIdentifier(node[3]))
+  t.false(S.isAtom(node[3]))
 
   t.true(S.isBoolean(node[4]))
   t.true(S.valueOf(node[4]))
@@ -83,28 +84,26 @@ test('type checks', function (t) {
   t.false(S.isString(node[6]))
 })
 
-
-
-test('parsing', function (t) {
+test("parsing", function (t) {
   t.plan(1)
   const S = new SExpr()
 
   const node = S.expression()
   node.push(
-    S.identifier('repeat'),
+    S.identifier("repeat"),
     S.number(3),
     // S.number(3),
-    S.expression(S.identifier('print'), S.string('Knock'))
+    S.expression(S.identifier("print"), S.string("Knock"))
   )
-  
-  const parsed = S.parse(`(repeat 3 (print "Knock"))`)
-  
-  // console.assert(S.isEqual(node, parsed))
-  
+
+  const ast = S.parse(`(repeat 3 (print "Knock"))`)
+
+  // console.assert(S.isEqual(node, ast))
+
   console.log(node)
-  
-  // same as S.isIdentifier(node[0]) && node[0] === 'repeat'
-  if (S.isIdentifier(S.first(node), 'repeat')) {  
+
+  // same as S.isAtom(node[0]) && node[0] === 'repeat'
+  if (S.isAtom(S.first(node), "repeat")) {
     if (node.length != 3) {
       console.error(`'repeat' expects 2 arguments: a number and an expression`)
       return
@@ -113,7 +112,7 @@ test('parsing', function (t) {
       const n = S.second(node) // same as node[1]
       const e = S.third(node) // same as node[2]
       if (S.isExpression(e)) {
-        if (S.isIdentifier(S.first(e), 'print') && S.isString(S.second(e))) {
+        if (S.isAtom(S.first(e), "print") && S.isString(S.second(e))) {
           for (let i = 0; i < n; ++i) {
             console.log(S.valueOf(S.second(e))) // S.valueOf is necessary for obtaining String value
           }
@@ -124,10 +123,119 @@ test('parsing', function (t) {
         console.error(`'repeat' expects an expression to be repeated`)
       }
     } else {
-      console.error(`'repeat' expects a number of times to repeat an expression`)
+      console.error(
+        `'repeat' expects a number of times to repeat an expression`
+      )
     }
   }
 
-  t.pass('parse normally')
-  
+  t.pass("parse normally")
+})
+
+test("evaluate", function (t) {
+  const S = new SExpr()
+  let testCases = [
+    {
+      note: "root list",
+      input: "(a b c)",
+      expect: [{ a: "a" }, { b: "b" }, { c: "c" }],
+    },
+    {
+      input: "((a b c))",
+      expect: [{ a: [{ b: "b" }, { c: "c" }] }],
+    },
+    {
+      input: "((a b c) (d e f))",
+      expect: [
+        { a: [{ b: "b" }, { c: "c" }] },
+        { d: [{ e: "e" }, { f: "f" }] },
+      ],
+    },
+    {
+      input: "(a (b c d) e (f) () (((g)) h))",
+      expect: [
+        { a: "a" },
+        { b: [{ c: "c" }, { d: "d" }] },
+        { e: "e" },
+        { f: [] },
+        { "": [] },
+        { g: [{ h: "h" }] },
+      ],
+    },
+    {
+      note: "multiline string",
+      input: '(a "multi\n line\n string")',
+      expect: [
+        {
+          a: "a",
+        },
+        "multi\n line\n string",
+      ],
+    },
+  ]
+  t.plan(testCases.length)
+  for (let testCase of testCases) {
+    if (testCase.note) {
+      console.log("Test   : " + testCase.note)
+    }
+    let input = testCase.input
+    console.log("Input  : " + JSON.stringify(input))
+
+    let ast = S.parse(input)
+    // console.dir(ast)
+    let output = S.evaluate(ast)
+    console.log(
+      "Output : " +
+        colorize(output, {
+          pretty: true,
+        })
+    )
+
+    let expect = testCase.expect
+    console.log("Expect : " + JSON.stringify(expect))
+
+    t.deepEqual(output, expect)
+    console.log("---")
+  }
+})
+
+test("comments", function (t) {
+  const S = new SExpr()
+  let testCases = [
+    {
+      note: "trailing comment after expression",
+      input: "(a b c) ; this is ignored",
+      expect: S.serialize(S.parse(`(a b c)`)),
+    },
+    {
+      note: "trailing comment in middle of expression",
+      input: `(a ; this is ignored 
+        b ; this is ignored too         
+        c)`,
+      expect: S.serialize(S.parse(`(a b c)`)),
+    },
+
+    {
+      note: "comment syntax in a string",
+      input: `(a "; this is still a string" b c)`,
+      expect: S.serialize(S.parse(`(a "; this is still a string" b c)`)),
+    },
+  ]
+  t.plan(testCases.length)
+  for (let testCase of testCases) {
+    if (testCase.note) {
+      console.log("Note   : " + testCase.note)
+    }
+    let input = testCase.input
+    console.log("Input  : " + JSON.stringify(input))
+
+    let output = S.serialize(S.parse(input))
+    console.log("Output : " + JSON.stringify(output))
+
+    let expect = testCase.expect
+    console.log("Expect : " + JSON.stringify(expect))
+
+    t.deepEqual(output, expect)
+    console.log("---")
+  }
 })
